b22851f0f522e3149772b4a0c64d2467d2b3617d
mtgomez <owl975@gmail.com>
2015-11-27 07:51:50 -0800

    synchronized, timout working

--- controllers/storyCtrl.js
+++ controllers/storyCtrl.js
@@ -1,20 +1,23 @@
 angular.module('storyBoard', [])
 
-  .controller('storyCtrl', ['$scope', function ($scope){
+  .controller('storyCtrl', ['$scope', '$timeout', function ($scope, $timeout){
  	$scope.storyTitle = "";
     $scope.storyContent = "";
     $scope.stories = {};
 
-    $scope.myData = new Firebase("https://shining-inferno-9788.firebaseio.com/stories");
+    $scope.myData = new Firebase("https://shining-inferno-9788.firebaseio.com/Stories");
 
     $scope.saveStory = function(){
-      $scope.myData.push({storyTitle:$scope.storyTitle, storyContent:$storyContent});
+      $scope.myData.push({storyTitle:$scope.storyTitle, storyContent:$scope.storyContent});
       $scope.storyTitle = "";
       $scope.storyContent = "";
     };
-
-    $scope.myData.on('value', function(snapshot){
-    	$scope.stories=snapshot.val();
+   
+    $scope.myData.on('value', function(snapshot) {
+    	$timeout(function(){
+    		$scope.stories = snapshot.val();
+    	});
     });
+     
 
   }]);
